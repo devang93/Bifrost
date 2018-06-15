ba26bad4645dff73f68a9e7d725896e962019460
Devang Anilkumar Patel <depatel@depatel012657.starbucks.net>
2018-06-15 10:05:18 -0700

    removed unnecessary comments.

--- src/main/scala/DataTransporter/GoogleBigQuerytoADLS.scala
+++ src/main/scala/DataTransporter/GoogleBigQuerytoADLS.scala
@@ -14,44 +14,7 @@ import org.slf4j.LoggerFactory
 object GoogleBigQuerytoADLS {
 
   private val log = LoggerFactory.getLogger("GoogleBigQueryToAdls")
-
-//  def main(args: Array[String]): Unit = {
-//    val credentialsPath = "google_cred.json"
-//    var credentials: ServiceAccountCredentials = ServiceAccountCredentials.fromStream(new FileInputStream(credentialsPath))
-//    val bg = BigQueryOptions.newBuilder().setCredentials(credentials).setProjectId("meta-matrix-90116").build().getService();
-//    val table = "ga_sessions_20180611"
-//    val dataSetName = "127577860"
-//    val uri = "gs://cdap_export/test_ga_sessions_uo/20180613/test_extract_dpatel-*.json.gz"
-//    val tableId: TableId = TableId.of(dataSetName, table)
-//
-//    val extractJobConfiguration = ExtractJobConfiguration.newBuilder(tableId, uri).setCompression("GZIP").setFormat("NEWLINE_DELIMITED_JSON").build()
-//
-//    val jobInfo: JobInfo = JobInfo.newBuilder(extractJobConfiguration).setJobId(JobId.of(UUID.randomUUID().toString)).build()
-//
-//    var job: Job = null
-//    try {
-//      job = bg.create(jobInfo)
-//      println(job.getJobId.getJob)
-//      println("Job Status: "+ job.getStatus.getState)
-//      val j = job.waitFor()
-//      println(j.getStatus.getState)
-//    } catch {
-//      case e: BigQueryException => println("JOB failed")
-//      case e: Exception => println("Other Exception!"); e.printStackTrace()
-//    }
-//
-//    val spark = SparkSession.builder()
-//      .config("fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
-//      .config("fs.gd.project.id", "meta-matrix-90116")
-//      .config("google.cloud.auth.service.accound.enable", "true")
-//      .config("google.cloud.auth.service.account.json.keyfile", "google_cred.json")
-//      .appName("googlecloud_to_adls")
-//        .master("yarn")
-//      .getOrCreate()
-//
-//    val raw = spark.read.json("gs://cdap_export/test_ga_sessions_uo/20180613/test_extract_dpatel-*.json.gz")
-//  }
-
+  
   def execute(conf: ApplicationConf): Unit = {
 
     val table = conf.bqtableName()
